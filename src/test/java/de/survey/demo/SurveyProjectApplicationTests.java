19eca671a93aa480e3af8827e00ee9d4c4b39616
lukas <lukas0606@outlook.de>
2025-09-24 17:40:19 +0200

    init

---
+++ src/test/java/de/survey/demo/SurveyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.survey.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
